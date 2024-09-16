55441ddc4f2fa2d7e20065a7301a9daae6168c3d
AndresGutierrezHurtado <andres52885241@gmail.com>
2024-09-16 03:40:30 -0500

    build: add cors and change icons

--- database/index.js
+++ database/index.js
@@ -1,6 +1,11 @@
 require('dotenv').config();
 const express = require('express');
+const cors = require('cors');
 const app = express();
+
+// Config
+app.use(express.json());
+app.use(cors());
 
 // Routes
 const userRoutes = require('./routes/user.routes');
