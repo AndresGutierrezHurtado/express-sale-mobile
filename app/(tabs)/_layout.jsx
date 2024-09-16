149f277bb63e485ae040448aa3060a71babc6b86
AndresGutierrezHurtado <andres52885241@gmail.com>
2024-09-15 22:33:33 -0500

    feat: add icons

--- app/(tabs)/_layout.jsx
+++ app/(tabs)/_layout.jsx
@@ -1,23 +1,24 @@
 import { Tabs } from "expo-router";
+import { HomeIcon, BagIcon, CartIcon, UserIcon } from "../../components/icons";
 
 export default function TabsLayout() {
     return (
         <Tabs>
             <Tabs.Screen
                 name="index"
-                options={{ title: "Inicio", tabBarIcon: () => null }}
+                options={{ title: "Inicio", tabBarIcon: () => <HomeIcon /> }}
             />
             <Tabs.Screen
                 name="products"
-                options={{ title: "Productos", tabBarIcon: () => null }}
+                options={{ title: "Productos", tabBarIcon: () => <BagIcon /> }}
             />
             <Tabs.Screen
                 name="cart"
-                options={{ title: "Carrito", tabBarIcon: () => null }}
+                options={{ title: "Carrito", tabBarIcon: () => <CartIcon /> }}
             />
             <Tabs.Screen
                 name="profile"
-                options={{ title: "Perfil", tabBarIcon: () => null }}
+                options={{ title: "Perfil", tabBarIcon: () => <UserIcon /> }}
             />
         </Tabs>
     );
---
+++ components/icons.jsx
@@ -0,0 +1,10 @@
+import { FontAwesome } from "@expo/vector-icons";
+
+// TabIcons (Home, shoppingBag, Cart, user)
+export const HomeIcon = (props) => <FontAwesome name="home" size={24} color={"black"} {...props} />
+
+export const BagIcon = (props) => <FontAwesome name="shopping-bag" size={24} color={"black"} {...props} />
+
+export const CartIcon = (props) => <FontAwesome name="shopping-cart" size={24} color={"black"} {...props} />
+
+export const UserIcon = (props) => <FontAwesome name="user" size={24} color={"black"} {...props} />
