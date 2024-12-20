b5ccff5b722410db580a8253e912ecd5c2aef9f4
AndresGutierrezHurtado <andres52885241@gmail.com>
2024-12-20 15:25:22 -0500

    style: improve layout

--- client/app/_layout.jsx
+++ client/app/_layout.jsx
@@ -1,4 +1,14 @@
+import { SafeAreaView } from "react-native-safe-area-context";
+import { StatusBar } from "react-native";
 import { Slot } from "expo-router";
+
 import "../global.css";
 
-export default Slot;
+export default function RootLayout() {
+    return (
+        <SafeAreaView className="flex-1 bg-white">
+            <StatusBar backgroundColor={"#fff"} barStyle={"dark-content"} />
+            <Slot />
+        </SafeAreaView>
+    );
+}
