a6579f0c7c9df067847851b7596441423d2c62db
AndresGutierrezHurtado <andres52885241@gmail.com>
2024-12-21 13:24:59 -0500

    build: add morgan

--- server/src/index.js
+++ server/src/index.js
@@ -1,4 +1,5 @@
 import express from "express";
+import morgan from "morgan";
 import cors from "cors";
 import session from "express-session";
 import * as models from "./models/index.js";
@@ -14,6 +15,7 @@ const app = express();
 
 // Middlewares
 app.use(express.json({ limit: "50mb" }));
+app.use(morgan("dev"));
 app.use(
     cors({
         origin: process.env.EXPO_PUBLIC_APP_DOMAIN,
