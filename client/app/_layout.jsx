0ede48761bded284f9540e6c0c92aa1bf9bbfdee
AndresGutierrezHurtado <andres52885241@gmail.com>
2024-12-21 02:11:22 -0500

    improve: update layout

--- client/app/_layout.jsx
+++ client/app/_layout.jsx
@@ -1,6 +1,6 @@
 import { SafeAreaView } from "react-native-safe-area-context";
 import { StatusBar } from "react-native";
-import { Slot } from "expo-router";
+import { Stack } from "expo-router";
 
 import "../global.css";
 
@@ -8,7 +8,9 @@ export default function RootLayout() {
     return (
         <SafeAreaView className="flex-1 bg-white">
             <StatusBar backgroundColor={"#fff"} barStyle={"dark-content"} />
-            <Slot />
+            <Stack>
+                <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+            </Stack>
         </SafeAreaView>
     );
 }
