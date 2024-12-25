b5285ef2a4d884a6c1f5167db2d375d61bc3e219
AndresGutierrezHurtado <andres52885241@gmail.com>
2024-12-24 23:40:55 -0500

    feat: add login view

--- client/app/(auth)/login.jsx
+++ client/app/(auth)/login.jsx
@@ -1,6 +1,71 @@
 import React from "react";
-import { Text } from "react-native";
+import { Image, Pressable, Text, TextInput, View } from "react-native";
+import { Link } from "expo-router";
 
 export default function Login() {
-  return <Text>Login</Text>;
+    return (
+        <View className="flex-1 pt-[50px]">
+            <View className="flex-1 items-center justify-center">
+                <View className="grow items-center justify-center gap-3">
+                    <Image
+                        source={{ uri: `${process.env.EXPO_PUBLIC_APP_DOMAIN}/logo.png` }}
+                        style={{ width: 140, height: 110, objectFit: "contain" }}
+                    />
+                    <View className="items-center justify-center">
+                        <Text className="text-4xl font-extrabold tracking-tight">Express Sale</Text>
+                        <Text className="text-lg leading-tight">
+                            Si no tienes cuenta aún,{" "}
+                            <Link href="/register" className="text-purple-700 font-semibold">
+                                regístrate
+                            </Link>
+                        </Text>
+                    </View>
+                </View>
+                <View
+                    className="h-fit w-full bg-white border border-gray-200 px-7 pt-12 pb-20 gap-10"
+                    style={{ borderTopLeftRadius: 30, borderTopRightRadius: 30 }}
+                >
+                    <View className="gap-2">
+                        <Text className="text-3xl font-extrabold text-center capitalize">
+                            Inicia sesión
+                        </Text>
+                        <Text className="text-lg text-gray-600 leading-none text-center max-w-xs mx-auto">
+                            Ingresa a tu cuenta para poder acceder a todas las funciones de la
+                            página
+                        </Text>
+                    </View>
+                    <View className="gap-3">
+                        <View className="gap-1">
+                            <Text className="text-lg font-semibold">Correo electrónico: </Text>
+                            <TextInput
+                                placeholder="ejemplo@gmail.com"
+                                className="w-full bg-white border px-3 py-1 rounded text-lg"
+                            />
+                        </View>
+                        <View className="gap-1">
+                            <Text className="text-lg font-semibold">Contraseña: </Text>
+                            <TextInput
+                                placeholder="********"
+                                className="w-full bg-white border px-3 py-1 rounded text-lg"
+                            />
+                        </View>
+                        <Link
+                            href="/recover"
+                            className="text-lg text-purple-700 font-semibold text-right"
+                        >
+                            Olvidaste tu contraseña?
+                        </Link>
+
+                        <View className="gap-2">
+                            <Pressable className="w-full bg-purple-700 py-2 rounded">
+                                <Text className="text-lg font-semibold text-center text-white">
+                                    Iniciar sesión
+                                </Text>
+                            </Pressable>
+                        </View>
+                    </View>
+                </View>
+            </View>
+        </View>
+    );
 }
