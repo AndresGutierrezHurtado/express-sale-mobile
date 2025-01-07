1008609f6fbc29e413cd5185cfd985122541148d
AndresGutierrezHurtado <andres52885241@gmail.com>
2025-01-06 19:04:24 -0500

    feat: add edit rating form

--- client/components/rating.jsx
+++ client/components/rating.jsx
@@ -1,13 +1,20 @@
 import React, { useState } from "react";
+import { Image, Modal, Pressable, ScrollView, Text, TextInput, View } from "react-native";
+import { Formik } from "formik";
+import { AntDesign } from "@expo/vector-icons";
 
-import { Image, Modal, Pressable, Text, View } from "react-native";
-import Stars from "./stars";
+// Hooks
+import { useDeleteData, usePutData } from "../hooks/useFetchData";
+import { useValidateForm } from "../hooks/useValidateForm";
+
+// Components
 import { DotsIcon, PencilIcon, ReportIcon, TrashIcon, XIcon } from "./icons";
-import { useDeleteData } from "../hooks/useFetchData";
+import Stars from "./stars";
 
-export default function Rating({ rating, reload }) {
+export default function Rating({ rating, reload, type }) {
     const [showOptions, setShowOptions] = useState(false);
     const [showEditRating, setShowEditRating] = useState(false);
+    const [errors, setErrors] = useState([]);
 
     const handleDeleteRating = async () => {
         const response = await useDeleteData(`/ratings/${rating.rating_id}`);
@@ -15,6 +22,18 @@ export default function Rating({ rating, reload }) {
         if (response.success) {
             setShowOptions(false);
             reload();
+        }
+    };
+
+    const handleSubmitEdit = async (values) => {
+        console.log(values);
+
+        const validation = useValidateForm(values, "rate-form");
+
+        setErrors(validation.errors || []);
+
+        if (validation.success) {
+            console.log(values);
         }
     };
 
@@ -113,6 +132,117 @@ export default function Rating({ rating, reload }) {
                         </View>
                     </View>
                 </View>
+            </Modal>
+            <Modal visible={showEditRating} animationType="slide" transparent>
+                <ScrollView className="flex-1 mt-[55vh] w-full bg-white rounded-t-[30px] border border-gray-400">
+                    <View className="gap-5 px-5 pt-10 pb-[50px]">
+                        <View className="flex-row justify-between items-center pr-5">
+                            <Text className="text-3xl font-extrabold">
+                                Calificar {type === "product" ? "producto" : "usuario"}
+                            </Text>
+                            <Pressable
+                                onPress={() => setShowEditRating(false)}
+                                className="active:bg-gray-300 w-9 h-9 rounded-full flex items-center justify-center"
+                            >
+                                <XIcon size={25} />
+                            </Pressable>
+                        </View>
+                        <Formik initialValues={{ rating_value: rating.rating_value, rating_comment: rating.rating_comment }} onSubmit={handleSubmitEdit}>
+                            {({
+                                handleChange,
+                                handleSubmit,
+                                setFieldValue,
+                                values,
+                            }) => (
+                                <View className="gap-5">
+                                    <View>
+                                        <Text className="text-lg leading-tight">
+                                            {type === "product"
+                                                ? `Ten en cuenta la calidad del producto y su fidelidad con la imagen y descripción.`
+                                                : `Ten en cuenta como es su comportamiento y calidad de servicio.`}
+                                        </Text>
+                                    </View>
+                                    <View className="gap-3">
+                                        <Text className="text-2xl font-extrabold">
+                                            Deja tu calificación:
+                                        </Text>
+                                        <View className="gap-1">
+                                            <View className="flex-row items-center justify-center">
+                                                {[1, 2, 3, 4, 5].map((star) => (
+                                                    <Pressable
+                                                        key={star}
+                                                        onPress={() =>
+                                                            setFieldValue("rating_value", star)
+                                                        }
+                                                    >
+                                                        <AntDesign
+                                                            name={
+                                                                star <= values.rating_value
+                                                                    ? "star"
+                                                                    : "staro"
+                                                            }
+                                                            size={32}
+                                                            color="#7E22CE"
+                                                            className="mx-1"
+                                                        />
+                                                    </Pressable>
+                                                ))}
+                                            </View>
+                                            {errors.find(
+                                                (error) => error.field === "rating_value"
+                                            ) && (
+                                                <Text className="text-red-600">
+                                                    {
+                                                        errors.find(
+                                                            (error) =>
+                                                                error.field === "rating_value"
+                                                        ).message
+                                                    }
+                                                </Text>
+                                            )}
+                                        </View>
+                                        <View className="flex-row gap-4 justify-between items-center">
+                                            <View className="gap-1 grow w-8/12">
+                                                <TextInput
+                                                    placeholder={`Ingresa tu comentario a cerca del ${
+                                                        type === "product" ? "producto" : "usuario"
+                                                    }`}
+                                                    className="bg-white border border-gray-400 rounded-lg px-3 py-2 text-lg h-[50px]"
+                                                    multiLine
+                                                    numberOfLines={4}
+                                                    value={values.rating_comment}
+                                                    onChangeText={handleChange("rating_comment")}
+                                                />
+                                                {errors.find(
+                                                    (error) => error.field === "rating_comment"
+                                                ) && (
+                                                    <Text className="text-red-600">
+                                                        {
+                                                            errors.find(
+                                                                (error) =>
+                                                                    error.field === "rating_comment"
+                                                            ).message
+                                                        }
+                                                    </Text>
+                                                )}
+                                            </View>
+                                            <View className="grow">
+                                                <Pressable
+                                                    onPress={handleSubmit}
+                                                    className="bg-purple-700 py-2 px-2 rounded"
+                                                >
+                                                    <Text className="text-lg font-semibold text-center text-white">
+                                                        Subir
+                                                    </Text>
+                                                </Pressable>
+                                            </View>
+                                        </View>
+                                    </View>
+                                </View>
+                            )}
+                        </Formik>
+                    </View>
+                </ScrollView>
             </Modal>
         </>
     );
